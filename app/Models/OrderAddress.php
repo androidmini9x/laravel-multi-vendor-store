8cec334249479be593e641d792c79653398d138e
Androidmini9x <androidmini92@gmail.com>
2024-02-23 19:42:50 +0300

    Add: some accessors

--- app/Models/OrderAddress.php
+++ app/Models/OrderAddress.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Intl\Countries;
 
 class OrderAddress extends Model
 {
@@ -14,4 +15,12 @@ class OrderAddress extends Model
     ];
 
     public $timestamps = false;
+
+    public function getNameAttribute() {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    public function getCountryNameAttribute() {
+        return Countries::getName($this->country); 
+    }
 }
