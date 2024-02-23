60a2f40402389601726243863f53fe9f86aecdbd
Androidmini9x <androidmini92@gmail.com>
2024-02-23 19:43:57 +0300

    Craete: Mail Notification

---
+++ app/Notifications/OrderCreatedNotification.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderCreatedNotification extends Notification
+{
+    use Queueable;
+
+
+    private $order;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $billing = $this->order->billingAddress;
+        
+        return (new MailMessage)
+                    ->subject("New Order #{$this->order->number}")
+                    ->from('no-reply@store.store', 'Customer Service')
+                    ->greeting("Hi, {$notifiable->name}")
+                    ->line("A new order #{$this->order->number} created by {$billing->name} from {$billing->country_name}.")
+                    ->action('Notification Action', url('/dashboard'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
